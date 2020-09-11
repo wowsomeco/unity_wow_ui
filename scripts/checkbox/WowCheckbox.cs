05e9a799c4788232d26a9442fff203ba3215857e
jim <irawan.jemmy@gmail.com>
2020-09-11 14:35:36 +0800

    added checkbox

---
+++ scripts/checkbox/WowCheckbox.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Wowsome {
+  namespace UI {
+    public class WowCheckbox : MonoBehaviour, IPointerClickHandler {
+      public delegate void EvTap(bool state);
+
+      public Image ImgCheck;
+
+      bool _state;
+      EvTap _onChange;
+
+      public bool State {
+        get { return _state; }
+        set {
+          _state = value;
+          ImgCheck.gameObject.SetActive(_state);
+        }
+      }
+
+      public void InitCheckbox(bool initialState, EvTap onChange) {
+        State = initialState;
+        _onChange = onChange;
+      }
+
+      public void OnPointerClick(PointerEventData eventData) {
+        State = !State;
+        _onChange(State);
+      }
+    }
+  }
+}
+
